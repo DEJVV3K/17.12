de17d75315508b745a268609b78b6e17cede84d7
DEJVV3K <62350376+DEJVV3K@users.noreply.github.com>
2020-12-17 11:16:03 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+void load() {
+	ifstream ciag;
+	ofstream wynik;
+	int d;
+	bool first = true;
+	int num,numl,num1,diff,diff1,diff2 = 0;
+	string a;
+	
+	ciag.open("ciagi.txt");
+	wynik.open("wynik1.txt");
+	while(ciag.good()){
+		bool aciag = true;
+		if(!ciag.eof()) {			
+			ciag >> num;
+			numl = num;
+			
+			for(int i = 0;i < numl;i++) {
+			 	ciag > >num;
+			 	
+			 	if(first == false) {
+			 	difffabs(num1-num);
+			 	}
+			 	
+			 	if(diff!=diff1 && i > 2) {
+				aciag = false;
+				
+				}
+				num1 = num;
+				first = false;
+				diff1 = diff;
+			}
+			
+			if(abs(diff1) > diff2){
+				diff2 = diff;
+			}
+		}
+	}
+	cout << diff << endl << d;
+	
+	ciag.close();
+}
+
+
+int main() {
+	load();
+	return 0;
+}
